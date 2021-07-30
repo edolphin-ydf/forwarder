1e8c78d1f8f894c2ed7cdaad1cd59d93d427eb1d
edolphin-ydf <shisanbihua@126.com>
2021-07-30 20:37:09 +0800

    2

--- main.go
+++ main.go
@@ -5,13 +5,45 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/http"
 	"strings"
 )
 
 var (
 	portsStr = flag.String("ports", "", "")
 	server = flag.String("server", "", "")
+	authKey = flag.String("auth", "", "")
 )
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if ak, exist := r.Header["Authorization"]; !exist {
+		log.Println("1")
+		return
+	} else if len(ak) == 0 {
+		log.Println("2")
+		return
+	} else if ak[0] != *authKey {
+		log.Println("3")
+		return
+	}
+
+	if r.URL.Path != "/updatesrv" {
+		log.Println("4")
+		return
+	}
+
+	if srv, exist := r.URL.Query()["srv"]; !exist {
+		log.Println("5")
+		return
+	} else if len(srv) == 0 {
+		log.Println("6")
+		return	
+	} else {
+		log.Println("set new server:", srv[0])
+		*server = srv[0]
+		return
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -56,6 +88,8 @@ func main() {
 		}(port)
 	}
 
-	ch := make(chan struct{}, 1)
-	<-ch
+	if err := http.ListenAndServe("0.0.0.0:19999", http.HandlerFunc(Handler)); err != nil {
+		log.Fatal(err)
+	}
 }
+
